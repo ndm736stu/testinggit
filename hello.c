8208dc62abb327cc8c03ecb9d916ae3eb6a7f6fe
ndm736ren <140965927+ndm736ren@users.noreply.github.com>
2023-07-30 10:42:21 -0500

    ren in new branch

--- hello.c
+++ hello.c
@@ -3,4 +3,10 @@
 int main(){
     printf("Hello World!\r\n");
     // ren adding a comment
+<<<<<<< Updated upstream
+=======
+    // stu adding a comment
+
+    // ren adding a comment in a new branch
+>>>>>>> Stashed changes
 }
